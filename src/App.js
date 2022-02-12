13f3843a624b7bb46183450a8cab0b914d10d284
David Lau <david.kaimun.lau@gmail.com>
2022-02-12 22:21:02 +0100

    added OnChange listeners to color pickers. TODO: update bg according to selected colors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import react, { Component } from "react";
 import ColorPicker from './ColorPicker';
 import KanyeQuoter from './KanyeQuoter';
+import './App.css';
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            color1: '',
+            color2: ''
+        }
+    }
+    OnColorPickerChange = (event) => {
+        if (event.target.className === "color1") {
+            this.setState({ color1: event.target.value})
+        } else if (event.target.className === "color2") {
+            this.setState({ color2: event.target.value})
+        }
+        console.log(this.state)
+    }
     render() {
+        const { color1, color2 } = this.state;
         return (
             <>
-            <div className='tc'>
+            <div className='tc' style={{background: 'linear-gradient(to right, ${color1}, ${color2})'}}>
                 <h1>Gradient Background Generator</h1>
-                <ColorPicker />
+                <ColorPicker colorChange={this.OnColorPickerChange}/>
                 <KanyeQuoter />
             </div>
             </>
-            
         )
     }
 }
--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,16 +1,21 @@
-import { render } from "@testing-library/react";
-import react, { Component } from "react";
-
-class ColorPicker extends Component {
-    render() {
-        return(
-            <div>
-                <input type='color'></input>
-                <input type='color'></input><br/>
-                <button>I want some magic</button>
-            </div>
-        )
-    }
+const ColorPicker = ({ colorChange }) => {
+    return(
+        <div>
+            <input 
+            className="color1" 
+            type="color" 
+            name="color1"
+            onChange={colorChange}
+            ></input>
+            <input 
+            className="color2" 
+            type="color" 
+            name="color2"
+            onChange={colorChange}
+            ></input><br/>
+            <button>I want some magic</button>
+        </div>
+    )
 }
 
 export default ColorPicker;
